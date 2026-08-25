da5e41042d0458738a98fbd4feaf1f8d8ec0942d
Healoz <75352596+Healoz@users.noreply.github.com>
2026-08-25 16:04:59 +1000

    Removing legacy javascript

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '../build/polyfills/polyfill-module': false,
+      'next/dist/build/polyfills/polyfill-module': false,
+    }
+    return config
+  },
   reactCompiler: true,
   experimental: {
     inlineCss: true,
